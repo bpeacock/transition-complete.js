29e516e3deb1341b61fbee6a027b9a72b61d270b
Brian Peacock <bpeacock@fastfig.com>
2014-05-01 11:08:00 -0500

    Added better exporting for different environments

--- transition-complete.js
+++ transition-complete.js
@@ -1,43 +1,62 @@
-var callbacks = [];
+(function(root) {
+    var callbacks = [];
 
-module.exports = function(callback) {
-    if(callbacks.length === 0) {
-        setEvent();
-    }
-
-    callbacks.push(callback);
-};
-
-function setEvent() {
-    document.addEventListener(eventName(), function() {
-        var i = callbacks.length;
-        while(i--) {
-            callbacks[i]();
+    var transitionComplete = function(callback) {
+        if(callbacks.length === 0) {
+            setEvent();
         }
 
-        callbacks = [];
-    });
-}
+        callbacks.push(callback);
+    };
 
-var _eventName;
+    function setEvent() {
+        document.addEventListener(eventName(), function() {
+            var i = callbacks.length;
+            while(i--) {
+                callbacks[i]();
+            }
 
-function eventName() {
-    if(!_eventName) {
-        // Sourced from: http://stackoverflow.com/questions/5023514/how-do-i-normalize-css3-transition-functions-across-browsers
-        var el = document.createElement('fakeelement');
-            transitions = {
-                transition:       'transitionend',
-                OTransition:      'oTransitionEnd',
-                MozTransition:    'transitionend',
-                WebkitTransition: 'webkitTransitionEnd'
-            };
+            callbacks = [];
+        });
+    }
 
-        for(var t in transitions) {
-            if(el.style[t] !== undefined) {
-                _eventName = transitions[t];
+    var _eventName;
+
+    function eventName() {
+        if(!_eventName) {
+            // Sourced from: http://stackoverflow.com/questions/5023514/how-do-i-normalize-css3-transition-functions-across-browsers
+            var el = document.createElement('fakeelement');
+                transitions = {
+                    transition:       'transitionend',
+                    OTransition:      'oTransitionEnd',
+                    MozTransition:    'transitionend',
+                    WebkitTransition: 'webkitTransitionEnd'
+                };
+
+            for(var t in transitions) {
+                if(el.style[t] !== undefined) {
+                    _eventName = transitions[t];
+                }
             }
         }
-    }
 
-    return _eventName;
-}
+        return _eventName;
+    }
+    
+    /*** Export ***/
+
+    // AMD
+    if(typeof define === 'function' && define.amd) {
+        define([], function() {
+          return transitionComplete;
+        });
+    }
+    // CommonJS
+    else if(typeof exports !== 'undefined') {
+        module.exports = transitionComplete;
+    }
+    // Browser Global
+    else {
+        root.transitionComplete = transitionComplete;
+    }
+})(this);
